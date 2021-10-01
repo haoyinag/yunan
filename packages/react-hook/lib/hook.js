0eabba9ee5f1d292df867bf1cdb5bb6ef9e3b3a5
Yu Nan <haoyinag0@hotmail.com>
2021-10-01 18:36:17 +0800

    update

--- packages/react-hook/lib/hook.js
+++ packages/react-hook/lib/hook.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-10-01 16:51:48
+ * @LastEditTime: 2021-10-01 18:36:11
+ * @FilePath: \yunan\packages\react-hook\lib\hook.js
+ * @Description: 
+ */
 'use strict';
 
 module.exports = hook;
 
 function hook() {
     // TODO
+    console.log('hook');
 }
