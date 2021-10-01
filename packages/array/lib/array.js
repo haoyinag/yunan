a9573b2a8f885b918a076335177ed359790de91e
Yu Nan <haoyinag0@hotmail.com>
2021-10-01 20:51:29 +0800

    Update array.js

--- packages/array/lib/array.js
+++ packages/array/lib/array.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-10-01 19:56:46
+ * @LastEditTime: 2021-10-01 20:51:26
+ * @FilePath: \yunan\packages\array\lib\array.js
+ * @Description: 
+ */
 'use strict';
 
 module.exports = array;
 
 function array() {
-    // TODO
+    // TODO 1
 }
