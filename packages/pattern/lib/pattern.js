ead03e61b7af367ffea57487c8fbc66f39f9d6a1
Yu Nan <haoyinag0@hotmail.com>
2021-10-01 17:40:43 +0800

    feature: update

--- packages/pattern/lib/pattern.js
+++ packages/pattern/lib/pattern.js
@@ -1,7 +1,15 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-10-01 16:50:49
+ * @LastEditTime: 2021-10-01 17:40:29
+ * @FilePath: \yunan\packages\pattern\lib\pattern.js
+ * @Description: 
+ */
 'use strict';
 
 module.exports = pattern;
 
 function pattern() {
-    // TODO
+    // TODO 
 }
