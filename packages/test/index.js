500c0f68b84b61806652d4c538e95076396e4bee
郁南 <yunan.chen@hairobotics.com>
2021-10-08 23:58:38 +0800

    1

---
+++ packages/test/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-10-08 23:37:44
+ * @LastEditTime: 2021-10-08 23:37:46
+ * @FilePath: /yunan/packages/test/index.js
+ * @Description: 
+ */
+console.log('test');
