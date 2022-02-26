a360d32ebd0de1d0b908d72ef019120455498615
郁南 <haoyinag0@hotmail.com>
2022-02-26 19:10:25 +0800

    feat(1): aa ss
    vv

--- cli/test.js
+++ cli/test.js
@@ -2,8 +2,8 @@
  * @Author: 郁南
  * @LastEditors: 郁南
  * @Date: 2022-02-26 18:13:34
- * @LastEditTime: 2022-02-26 18:37:38
+ * @LastEditTime: 2022-02-26 19:08:38
  * @FilePath: \yunan\cli\test.js
  * @Description: 
  */ 
-console.log('test husky 哈哈哈')
+console.log('test husky 哈哈哈 aaa')
