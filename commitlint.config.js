bc16f0fa88c4b430537ebae49f55c331a0c12c97
郁南 <haoyinag0@hotmail.com>
2022-02-26 19:07:58 +0800

    feat(a): b c d

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2022-02-26 18:25:44
+ * @LastEditTime: 2022-02-26 18:57:22
+ * @FilePath: \yunan\commitlint.config.js
+ * @Description: git 提交校验规则
+ */
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+};
