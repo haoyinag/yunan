612d0bcd0ce2a274a0176245c19488a687f6df9f
郁南 <haoyinag0@hotmail.com>
2022-02-26 18:17:59 +0800

    Keep calm and commit

---
+++ cli/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2022-02-26 18:13:34
+ * @LastEditTime: 2022-02-26 18:13:45
+ * @FilePath: \yunan\cli\test.js
+ * @Description: 
+ */ 
+console.log('test husky')
